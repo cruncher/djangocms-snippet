c1643f51d1feec42f41a690930628cb3aead6293
Marco Bonetti <marco@cruncher.ch>
2017-11-16 12:05:21 +0100

    use a proper dict to render the snippet

--- djangocms_snippet/templatetags/snippet_tags.py
+++ djangocms_snippet/templatetags/snippet_tags.py
@@ -91,10 +91,10 @@ class SnippetFragment(template.Node):
                 context.update({
                     'html': mark_safe(instance.html)
                 })
-                content = t.render(template.Context(context))
+                content = t.render(context)
             else:
                 t = template.Template(instance.html)
-                content = t.render(template.Context(context))
+                content = t.render(context)
         except template.TemplateDoesNotExist:
             content = _('Template %(template)s does not exist.') % {
                 'template': instance.template}
