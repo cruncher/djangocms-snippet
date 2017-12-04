5a80f37ec35268c5658ccd6f41c35eeb1a8cf387
Marco Bonetti <marco@cruncher.ch>
2017-12-04 17:07:30 +0100

    snipped without cache

--- djangocms_snippet/cms_plugins.py
+++ djangocms_snippet/cms_plugins.py
@@ -17,6 +17,7 @@ class SnippetPlugin(CMSPluginBase):
     render_template = 'djangocms_snippet/snippet.html'
     text_enabled = True
     text_editor_preview = False
+    cache = False
 
     def render(self, context, instance, placeholder):
         render_context = {}
